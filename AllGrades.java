aef99aeb46cc76241a61c6b86559ce88a6587cdc
Osaede4greg <82848732+Osaede4greg@users.noreply.github.com>
2021-05-05 21:01:35 -0400

    Add files via upload

---
+++ AllGrades.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AllGrades {
+	
+	public static double calcAverage( double score1, double score2, double score3, double score4, double score5, double score6, double score7, double score8, double score9, double score10 ) {
+		double average;
+	    average = ( score1 + score2 + score3 + score4 + score5 +score6 + score7 + score8 + score9 + score10 ) / 10;
+	    return average;
+	    
+	
+
+	public static void main (String) [] args) {
+	
+	String[] names = {"Toni, Bryan, Chris, Shelby, Alex, Tyler, Erin, Ben, Tiffany, Sarah"};
+	
+	int[] grades = { 89,90,80,100,95,70,65,100,72,56};
+	
+		// TODO Auto-generated method stub
+	
+	}
