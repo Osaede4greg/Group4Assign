8a96673954e67bb27b440615c0e1f1d31db21f6b
Osaede4greg <82848732+Osaede4greg@users.noreply.github.com>
2021-05-05 22:23:37 -0400

    Add files via upload

---
+++ HighestScore.java
@@ -0,0 +1,61 @@
+
+public class HighestScore {
+ int highscore;
+ int lowscore;
+ int fail;
+ int highestscore;
+ String name;
+ public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] name = {"Toni","Bryan","Chris","Shelby","Alex","Tyler","Erin","Ben","Tiffany","Sarah"};
+		
+		
+				int[] highscore = {95,90,87,80};
+				int[] lowscore = {70, 72};
+				int[] fail = {56,65};
+				int highestscore =100;
+				 int[]num = new int[10];
+				
+				 num [0]= 87;
+				 num [1]= 90;
+				 num [2]= 80;
+				 num[3]= 100;
+				 num[4]= 95;
+				 num [5] = 70;
+				 num [6]= 65;
+				 num[7] =100;
+				 num[8]= 72;
+				 num[9] = 56;
+				if(highestscore == num[7])
+				{
+					System.out.println("Congraulation you have the highest score" + name[7] + name [3] + num [7] + " "+ highestscore);	
+				}
+				
+							
+								{
+									
+									System.out.println("Congraulation you have the highest score" + name[1] + " " + num[1] + " " + highscore);
+									System.out.println("Congraulation you have the lowestscore" + name[6] + " " +  num[6] + " " + lowscore);
+									System.out.println("Congraulation you have the failing score" + name[9] + " " + num [9] + " " + fail);
+									
+									
+			
+}		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
